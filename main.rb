750c009eb438fb42ff08ffd19a2562e285c82f62
Rory <rory.bell@gaiaresources.com.au>
2020-06-20 16:10:42 +1000

    dropped out incomplete works

--- main.rb
+++ main.rb
@@ -12,16 +12,11 @@ class RequestHandler
       @base_request = HTTParty.get(@base_url, { :headers =>  @headers })
     end
 
-    def show
-      @response = @base_request
-      puts "Status: #{@response.code}, Body: #{@response.body}"
-    end
 end
 
 
 request_handler = RequestHandler.new($api_base_url, $api_token)
-user_request = UserRequest.new()
-user_request.show
+
 
 
 
