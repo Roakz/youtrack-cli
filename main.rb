75fabee0a8f012442758ad612624549d07a60de6
Rory <rory.bell@gaiaresources.com.au>
2020-06-21 14:50:18 +1000

    reshuffle of logic to assign HTTParty requests to a shortened method name.

--- main.rb
+++ main.rb
@@ -7,40 +7,48 @@ def help_docs
   puts "Youtrack-CLI Instructions for use\n  Command structure: youtrack <ENTITY i.e Issue or Project>  <METHOD/CRUD action>"
 end
 
+def get(query)
+  HTTParty.get(query, { :headers => {'Authorization' => $api_token, 'Accept' => 'application/json', 'Content-Type' => 'application/json'}})
+end
 
 class Project 
 
   attr_reader :index
 
   def index
-    response = HTTParty.get($api_base_url + "admin/projects?fields=id,name", { :headers =>  { 'Authorization' => $api_token, 'Accept' => 'application/json', 'Content-Type' => 'application/json'}})
-    results = JSON.parse(response.body)
-    results.each do |project|
-      puts "Project: #{project['name']}, ProjectId: #{project['id']}"
+    @response = get($api_base_url + "admin/projects?fields=id,name")
+    if @response.code == 200
+      @results = JSON.parse(@response.body)
+      @results.each do |project|
+        puts "Project: #{project['name']}, ProjectId: #{project['id']}"
+      end
+    else
+      @error = JSON.parse(@response.body)
+      puts "Error: #{@error['error']}"
     end
   end
 
   def show(id)
-    response = HTTParty.get($api_base_url + "admin/projects/#{id}?fields=name,shortName,description,createdBy,team(name,usersCount)", { :headers =>  { 'Authorization' => $api_token, 'Accept' => 'application/json', 'Content-Type' => 'application/json'}})
-    results = JSON.parse(response.body)
-    puts "Project name: #{results['name']}" 
-    puts "ShortName: #{results['shortName']}"
-    puts "Created By: #{results['createdBy']}" unless !results['createdBy'] 
-    puts "Team: #{results['team']['name']} with user count of #{results['team']['usersCount']}" unless !results['team']
-    puts "Description: #{results['description']}"
+    @response = get($api_base_url + "admin/projects/#{id}?fields=name,shortName,description,createdBy,team(name,usersCount)")
+    @results = JSON.parse(@response.body)
+    puts "Project name: #{@results['name']}" 
+    puts "ShortName: #{@results['shortName']}"
+    puts "Created By: #{@results['createdBy']}" unless !@results['createdBy'] 
+    puts "Team: #{@results['team']['name']} with user count of #{@results['team']['usersCount']}" unless !@results['team']
+    puts "Description: #{@results['description']}"
   end
 end
 
 if ARGV.length > 0
-  unless %w(show update delete create).include?(ARGV[1])
+  if !%w(show update delete create).include?(ARGV[1])
     method_to_call = ARGV[1]
     class_to_call = Object.const_get(ARGV[0]).new()
     class_to_call.public_send(method_to_call) if class_to_call.respond_to? method_to_call
-    return
+  else
+    method_to_call = ARGV[1]
+    class_to_call = Object.const_get(ARGV[0]).new()
+    class_to_call.public_send(method_to_call, ARGV[2]) if class_to_call.respond_to? method_to_call
   end
-  method_to_call = ARGV[1]
-  class_to_call = Object.const_get(ARGV[0]).new()
-  class_to_call.public_send(method_to_call, ARGV[2]) if class_to_call.respond_to? method_to_call
 else   
   help_docs()  
 end
